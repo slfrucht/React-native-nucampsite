d17583294bebb2f6174259063fde8acb10a58af6
slfrucht <sfrucht@lmi.net>
2021-05-17 12:39:55 -0700

    add about and contact components

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,6 +2,8 @@ import React, {Component} from "react";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 import {View, Platform} from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
@@ -10,7 +12,9 @@ import { createDrawerNavigator } from "react-navigation-drawer";
 const DirectoryNavigator = createStackNavigator(
     {
         Directory: {screen: Directory},
-        CampsiteInfo: {screen: CampsiteInfo}
+        CampsiteInfo: {screen: CampsiteInfo},
+        Contact: {screen: Contact},
+        About: {screen: About}
     },
     {
         initialRouteName: "Directory",
@@ -45,10 +49,47 @@ const HomeNavigator = createStackNavigator(
 
 );
 
+const AboutNavigator = createStackNavigator(
+    {
+        About: {screen: About}
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: "#5637DD"
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+                color:"#fff"
+            }
+        }
+    }
+
+);
+const ContactNavigator = createStackNavigator(
+    {
+        Contact: {screen: Contact}
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: "#5637DD"
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+                color:"#fff"
+            }
+        }
+    }
+
+);
+
 const MainNavigator = createDrawerNavigator(
     {
         Home: {screen: HomeNavigator},
-        Directory: {screen: DirectoryNavigator}
+        Directory: {screen: DirectoryNavigator},
+        Contact: {screen: ContactNavigator},
+        About: {screen: AboutNavigator}
     },
     {
         drawerBackgroundColor: "#CEC8FF"
