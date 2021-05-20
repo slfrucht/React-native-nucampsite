d4b6508fcd4842dd0f9ae1f3bc83f59df1320dc9
slfrucht <sfrucht@lmi.net>
2021-05-20 16:48:47 -0700

    add reservation modal

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import CampsiteInfo from "./CampsiteInfoComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
+import Reservation from "./ReservationComponent";
 import {View, Platform, StyleSheet, Text, ScrollView, Image } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
@@ -123,6 +124,31 @@ const ContactNavigator = createStackNavigator(
     }
 
 );
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: {screen: Reservation}
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: "#5637DD"
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+                color:"#fff"
+            },
+            headerLeft: <Icon
+            name="tree"
+            type="font-awesome"
+            iconStyle={styles.stackIcon}
+            onPress={() => navigation.toggleDrawer()}
+            />
+
+        })
+    }
+
+);
+
 
 const CustomDrawerContentComponent = props => (
     <ScrollView>
@@ -176,13 +202,13 @@ const MainNavigator = createDrawerNavigator(
             }
 
         },
-        Contact: {
-            screen: ContactNavigator,
+        Reservation: {
+            screen: ReservationNavigator,
             navigationOptions: {
-                drawerLabel: "Contact Us",
+                drawerLabel: "Reserve Campsite",
                 drawerIcon: ({tintColor}) => (
                     <Icon
-                    name="address-card"
+                    name="tree"
                     type="font-awesome"
                     size={24}
                     color={tintColor}
@@ -191,6 +217,7 @@ const MainNavigator = createDrawerNavigator(
             }
 
         },
+
         About: {
             screen: AboutNavigator,
             navigationOptions: {
@@ -198,6 +225,22 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({tintColor}) => (
                     <Icon
                     name="info-circle"
+                    type="font-awesome"
+                    size={24}
+                    color={tintColor}
+                    />
+                )
+            }
+
+        },
+
+        Contact: {
+            screen: ContactNavigator,
+            navigationOptions: {
+                drawerLabel: "Contact Us",
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                    name="address-card"
                     type="font-awesome"
                     size={24}
                     color={tintColor}
