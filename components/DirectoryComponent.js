8d228860ebf580248371b2ae2fa09b0776f55119
slfrucht <sfrucht@lmi.net>
2021-05-25 15:45:38 -0700

    add animations to other pages

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import {FlatList, View, Text} from "react-native";
-import {Tile} from "react-native-elements";
+import { FlatList, View, Text } from "react-native";
+import { Tile } from "react-native-elements";
 import { connect } from "react-redux";
-import { baseUrl} from "../shared/baseUrl";
+import { baseUrl } from "../shared/baseUrl";
 import Loading from "./LoadingComponent";
+import * as Animatable from "react-native-animatable";
 
 const mapStateToProps = state => {
     return {
@@ -17,26 +18,27 @@ class Directory extends Component {
     };
 
     render() {
-        const {navigate} = this.props.navigation;
+        const { navigate } = this.props.navigation;
 
-        const renderDirectoryItem = ({item}) => {
-            return(
-                <Tile 
-                title={item.name}
-                caption={item.description}
-                featured
-                onPress={() => navigate("CampsiteInfo", {campsiteId: item.id})}
-                imageSrc={{uri: baseUrl + item.image}}
-                />
-    
+        const renderDirectoryItem = ({ item }) => {
+            return (
+                <Animatable.View animation="fadeInRightBig" duration={2000}>
+                    <Tile
+                        title={item.name}
+                        caption={item.description}
+                        featured
+                        onPress={() => navigate("CampsiteInfo", { campsiteId: item.id })}
+                        imageSrc={{ uri: baseUrl + item.image }}
+                    />
+                </Animatable.View>
             );
         };
-        if(this.props.campsites.isLoading) {
+        if (this.props.campsites.isLoading) {
             return (
                 <Loading />
             );
         }
-        else if(this.props.campsites.errMess) {
+        else if (this.props.campsites.errMess) {
             return (
                 <View>
                     <Text>{this.props.campsites.errMess}</Text>
@@ -45,13 +47,13 @@ class Directory extends Component {
         }
 
         return (
-            <FlatList 
-                data = {this.props.campsites.campsites}
-                renderItem = {renderDirectoryItem}
-                keyExtractor = {item => item.id.toString()}
+            <FlatList
+                data={this.props.campsites.campsites}
+                renderItem={renderDirectoryItem}
+                keyExtractor={item => item.id.toString()}
             />
         );
-    
+
     }
 }
 export default connect(mapStateToProps)(Directory);
